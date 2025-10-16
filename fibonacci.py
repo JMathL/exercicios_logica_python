d856f6b2a46c606bd0746befe9eada777acc9142
Matheus <thetheus.lim@gmail.com>
2025-10-16 17:41:04 -0300

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,13 @@
 numero = input("Quantas vezes quer: ")
+numero_int = int(numero)
 
-def calcular_fibonacci(numero):
-  for i in range(1, numero)
+contador = 0
+numero1 = 0
+numero2 = 1
+resultado = 0
+
+while contador <= numero_int:
+  contador += 1
+  resultado = numero1 + numero2
+  print(resultado, end=" ")
+  numero1 += numero2
